f344af0aa504ddd5e6cdff5a2795e9b70bc7d989
RizkyYanuar45 <rizkyanuar4@gmail.com>
2025-08-26 05:36:27 +0700

    push

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,7 +14,9 @@ exports.protectAdmin = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
     }
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized access" });
+      return res
+        .status(401)
+        .json({ message: "Token Kadaluwarsa Silahkan Login Ulang" });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
